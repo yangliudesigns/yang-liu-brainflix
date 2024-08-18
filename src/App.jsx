1a4599dca4c64939678d778c4e23a4f01bd06288
yangliudesigns <yangliudesigns@gmail.com>
2024-08-18 16:01:14 -0400

    cannot show the avatar and merge first

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import Button from "./components/Button/Button"
 import commentIcon from "./assets/images/add_comment.svg"
 
 
-
 export default function App() {
 
   return(
 
     <div>
 
+
     <Button type="submit" text="COMMENT" icon={commentIcon} />
+
+
 
 
   </div>
--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -29,3 +29,7 @@ export default function Avatar({ imgSrc }) {
 
 
 
+
+
+
+
