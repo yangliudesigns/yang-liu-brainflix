caf11c8910594b331df4e0c7888d3eb137afe36a
yangliudesigns <yangliudesigns@gmail.com>
2024-08-18 15:54:07 -0400

    Update avatar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import Button from "./components/Button/Button"
 import commentIcon from "./assets/images/add_comment.svg"
-import imgSrc from "./assets/images/Mohan-muruge.jpg"
-import Avatar from "./components/Avatar/Avatar"
+
 
 
 export default function App() {
@@ -11,10 +10,7 @@ export default function App() {
     <div>
 
     <Button type="submit" text="COMMENT" icon={commentIcon} />
-    <Button type="submit" text="COMMENT" icon={uploadIcon} />
 
-    <Avatar />
-    <Avatar imgSrc={imgSrc}/>
 
   </div>
   ) 
--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -4,14 +4,19 @@ import "./Avatar.scss";
 import React from 'react'
 
 export default function Avatar({ imgSrc }) {
-  return (
-    <img 
-    className="avatar" 
-    src={imgSrc} 
-    alt="This is user avatar" 
-    />
-  )
+    return (
+        <div className="default-avatar">
+            {
+                avatar ? (
+                <img className="avatar" src={imgScr} alt="This is user avatar" />
+                ) : (
+                <div></div>
+                )
+            }
+        </div>
+    )
 }
+
 
 
 
