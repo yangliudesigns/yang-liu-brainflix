bfb57de0eccb675a1f648eec53acbf5bce5622e7
yangliudesigns <yangliudesigns@gmail.com>
2024-08-18 13:37:39 -0400

    Finish Button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import Button from "./components/Button/Button"
+import commentIcon from "./assets/images/add_comment.svg"
 
 
 export default function App() {
 
-  return 
-  <div>
+  return(
 
-    <Button />
+    <div>
+
+    <Button type="submit" text="COMMENT" icon={commentIcon} />
 
   </div>
-    
-    
+  ) 
 
 }
 
