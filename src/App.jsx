f0e40099795aceb77b05aa04c108ff4013826b4d
yangliudesigns <yangliudesigns@gmail.com>
2024-08-18 02:53:46 -0400

    Add button jsx and scss detail

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import Button from "./components/Button/Button"
 
 
 export default function App() {
 
   return 
+  <div>
+
+    <Button />
+
+  </div>
+    
     
 
 }
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import './Button.scss';
+
+export default function Button({type, text, icon}) {
+  return (
+    <div>
+      <button className="btn">
+      {icon && <img className="btn-icon" src={icon} alt="icon" />}
+      {text}
+      </button>
+    </div>
+  )
+}
+
+
+
+
+
+
+// export default function Button({type, text, icon }) {
+//   return (
+//     <button type={type} className="btn">
+//       <img className="btn-icon" src={icon} alt="icon" />
+//       {text}
+//     </button>
+//   );
+// }
+
+// //navigation
+// import React from 'react';
+// import Button from '../Button/Button';
+// import uploadIcon from '../../assets/images/upload-icon.svg'; 
+
+// function Navigation() {
+//   return (
+//     <nav className="navigation">
+//         <a href="#">
+//             <Button text="UPLOAD" icon={uploadIcon} />
+//         </a>
+//     </nav>
+//   );
+// }
+
+// export default Navigation;
+
+// //comment section
+// import React from 'react';
+// import Button from '../Button/Button';
+// import commentIcon from '../../assets/images/comment-icon.svg'; 
+
+// function Comment() {
+//     return (
+//       <nav className="comment">
+//         <Button type="submit" text="COMMENT" icon={commentIcon} />
+//       </nav>
+//     );
+//   }
+  
+//   export default Comment;
