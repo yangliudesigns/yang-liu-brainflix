951877513b95be106712931603235ec3c278a277
yangliudesigns <yangliudesigns@gmail.com>
2024-08-19 10:08:41 -0400

    Update App scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,45 @@
-import Header from "./components/Header/Header"
-import VideoPlayer from "./components/VideoPlayer/VideoPlayer"
-import VideoInfo from "./components/VideoInfo/VideoInfo"
-import infodata from "./data/video-details.json";
+import Header from './components/Header/Header';
+import VideoPlayer from './components/VideoPlayer/VideoPlayer';
+import VideoInfo from './components/VideoInfo/VideoInfo';
+import infodata from './data/video-details.json';
 import React, { useState } from 'react';
-import VideoList from "./components/VideoList/VideoList"
-import CommentForm from "./components/CommentForm/CommentForm"
-import CommentList from "./components/CommentList/CommentList"
-
+import VideoList from './components/VideoList/VideoList';
+import CommentForm from './components/CommentForm/CommentForm';
+import CommentList from './components/CommentList/CommentList';
+import './App.scss';
 
 export default function App() {
-
   const [currentVideo, setCurrentVideo] = useState(infodata[0]);
 
   const handleVideoSelect = (videoId) => {
-    const selectedVideo = infodata.find(video => video.id === videoId);
+    const selectedVideo = infodata.find((video) => video.id === videoId);
     setCurrentVideo(selectedVideo);
   };
 
-
-  return(
-
-    <div>
-
+  return (
+    <div className="app">
       <Header />
+          <VideoPlayer
+            vidSrc={currentVideo.video}
+            posSrc={currentVideo.image}
+          />
 
-      <main>
-        <VideoPlayer 
-          vidSrc={currentVideo.video} 
-          posSrc={currentVideo.image}
-        />
-        <VideoInfo video={currentVideo} />
-        <VideoList 
-          vids={infodata} 
-          currentVideo={currentVideo} 
-          changeVideo={handleVideoSelect} 
-        />
-        <CommentForm />
-        <CommentList comments={currentVideo.comments} />
+      <main className="content">
+        <section className="main-content">
+          <VideoInfo video={currentVideo} />
+
+          <CommentForm />
+          <CommentList comments={currentVideo.comments} />
+        </section>
+
+        <section className="side-content">
+          <VideoList
+            vids={infodata}
+            currentVideo={currentVideo}
+            changeVideo={handleVideoSelect}
+          />
+        </section>
       </main>
-
-
-  </div>
-  ) 
-
+    </div>
+  );
 }
-
