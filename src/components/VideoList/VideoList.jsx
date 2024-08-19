2629d80ee36ae55bf67ec6d46aa9eaae3aae1da5
yangliudesigns <yangliudesigns@gmail.com>
2024-08-19 07:13:35 -0400

    Update video list

--- src/components/VideoList/VideoList.jsx
+++ src/components/VideoList/VideoList.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import "./VideoList.scss";
 
-export default function VideoList() {
+
+export default function VideoList({vids, currentVideo, changeVideo}) {
   return (
-    <div>
-      
-    </div>
+
+    <section className='video-list'>
+
+    <h6 className='video-list__header paragraph-labels'>
+      NEXT VIDEOS
+    </h6>
+
+    <div className='video-list__listing'>
+
+        {vids.filter(video => video.id !== currentVideo.id)
+        .map(video => {
+            return(
+                <video
+                key={video.id}
+                id={video.id}
+                title={video.title}
+                channel={video.channel}
+                image={video.image}
+                changeVideo={changeVideo}
+            />
+            )
+        })
+    }  </div>
+        </section>
   )
 }
