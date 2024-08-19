86d61863fdd448cc0986f25c0ac7b235112c2294
yangliudesigns <yangliudesigns@gmail.com>
2024-08-19 06:54:52 -0400

    Update video card

---
+++ src/components/VideoCard/VideoCard.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+
+
+
+export default function VideoCard({ 
+    id, 
+    title, 
+    channel, 
+    image, 
+    changeVideo 
+}) {
+
+  return (
+
+        <div className="video-card">
+
+            <a 
+            className="video-card__item"
+            onClick={() => {
+            changeVideo(id)
+            }} href='#root'>
+
+            <img 
+            className="video-card__item--image" 
+            src={image} 
+            alt="This is video thumbnail"
+            />
+
+            <div className="video-card__item--text-container">
+                <h6 className="video-card__item--title heading-subheader">
+                    {title}
+                </h6>
+                <p className="video-card__item--channel paragraph-body">
+                    {channel}
+                </p>
+            </div>
+            </a>
+
+        </div>
+
+  )
+}
