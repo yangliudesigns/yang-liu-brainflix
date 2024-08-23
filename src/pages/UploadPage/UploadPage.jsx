ab30789b8fc33ee6570e52f7a93511e4a1baffb7
yangliudesigns <yangliudesigns@gmail.com>
2024-08-23 01:36:00 -0400

    Fix Upload Page

--- src/pages/UploadPage/UploadPage.jsx
+++ src/pages/UploadPage/UploadPage.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import "./UploadPage.scss";
+import axios from 'axios';
 import UploadForm from "../../components/UploadForm/UploadForm";
-import thumbnail from "../../assets/images/image9.jpeg";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+
+
+const API_URL = "https://unit-3-project-api-0a5620414506.herokuapp.com";
+const API_KEY = "3997877e-6e9c-4457-a46b-b1eb77309298";
+
 
 
 
@@ -12,22 +17,25 @@ export default function UploadPage() {
     const navigate = useNavigate();
     const [uploadValues, setUploadValues] = useState(null);
 
-    useEffect(() => {
-        document.title = `BrainFlix: Upload`;
-    }, []);
-
     const handleUploadSubmitted = async (uploadData) => {
         const videoPostData = {
-        ...uploadData,
-        channel: "BrainFlix User",
-        thumbnailUrl: thumbnail,
+            ...uploadData,
+            channel: "BrainFlix User",
+            thumbnailUrl: "https://unit-3-project-api-0a5620414506.herokuapp.com/images/image9.jpeg",
         };
-        
-        setUploadValues(videoPostData);
-        setTimeout(() => navigate("/"), 3000);
-        };
-        
-        const handleCancel = () => navigate("/");
+
+        axios.post(`${API_URL}/videos?api_key=${API_KEY}`, videoPostData)
+        .then(() => {
+            setUploadValues({ ...uploadData });
+            setTimeout(() => navigate("/"), 3000);
+        })
+        .catch(error => {
+            console.error("Error uploading video:", error);
+        });
+    };
+
+    const handleCancel = () => navigate("/");
+    document.title = `BrainFlix: Upload`;
 
 
 
